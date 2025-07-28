503a6b0826e36b9166bda0294dabf3bd6c63bbe8
Joseph <jxlove2020@gmail.com>
2025-07-28 14:43:38 +0900

    정책 적용

--- modal.js
+++ modal.js
@@ -1,6 +1,14 @@
+function isMobile() {
+  return /Mobi|Android|iPhone/i.test(navigator.userAgent);
+}
+
 function openModal(pdfPath) {
-  document.getElementById('pdfFrame').src = pdfPath;
-  document.getElementById('pdfModal').style.display = 'flex';
+  if (isMobile()) {
+    window.open(pdfPath, '_blank'); // 새 창에서 열기
+  } else {
+    document.getElementById('pdfFrame').src = pdfPath;
+    document.getElementById('pdfModal').style.display = 'flex';
+  }
 }
 
 function closeModal() {
