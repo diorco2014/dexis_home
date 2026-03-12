4284fca33858c1b808afdc5a7a2bc43002e60922
yoon123456 <yw01124@gmail.com>
2026-03-12 11:40:48 +0900

    팝업 공지 내림

--- popup.js
+++ popup.js
@@ -1,23 +1,24 @@
 function getTodayString() {
-	const today = new Date();
-	return today.toISOString().slice(0, 10); // YYYY-MM-DD
+  const today = new Date();
+  return today.toISOString().slice(0, 10); // YYYY-MM-DD
 }
 
 function shouldShowPopup() {
-	const hiddenDate = localStorage.getItem("popupHiddenDate");
-	return hiddenDate !== getTodayString();
+  const hiddenDate = localStorage.getItem("popupHiddenDate");
+  return hiddenDate !== getTodayString();
 }
 
 function closePopup() {
-	const dontShow = document.getElementById("dont-show-today").checked;
-	if (dontShow) {
-		localStorage.setItem("popupHiddenDate", getTodayString());
-	}
-	document.getElementById("popup").style.display = "none";
+  const dontShow = document.getElementById("dont-show-today").checked;
+  if (dontShow) {
+    localStorage.setItem("popupHiddenDate", getTodayString());
+  }
+  document.getElementById("popup").style.display = "none";
 }
 
 window.onload = function () {
-	if (shouldShowPopup()) {
-		document.getElementById("popup").style.display = "block";
-	}
+  if (shouldShowPopup()) {
+    // 예원: 팝업 안보이게 처리 (2026-03-12) show : "block", hide: "none"
+    document.getElementById("popup").style.display = "none";
+  }
 };
