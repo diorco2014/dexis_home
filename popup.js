5756ad9f64de659baeca256d5a27581e632aa4bc
diobeen <diorcobeen@gmail.com>
2026-05-20 13:46:56 +0900

    0520 캠페인 팝업 이미지 변경

--- popup.js
+++ popup.js
@@ -15,13 +15,14 @@ function shouldShowPopup2() {
 
 function updateContainerVisibility() {
   const p1 = document.getElementById("popup");
-  const p2 = document.getElementById("popup2");
+  // const p2 = document.getElementById("popup2");
   const scroll = document.querySelector(".popup-scroll");
 
   const p1Hidden = p1.style.display === "none" || p1.style.display === "";
-  const p2Hidden = p2.style.display === "none" || p2.style.display === "";
+  // const p2Hidden = p2.style.display === "none" || p2.style.display === "";
 
-  scroll.style.display = p1Hidden && p2Hidden ? "none" : "";
+  // scroll.style.display = p1Hidden && p2Hidden ? "none" : "";
+  scroll.style.display = p1Hidden ? "none" : "";
 }
 
 function closePopup() {
@@ -54,10 +55,9 @@ window.onload = function () {
     document.getElementById("popup").style.display = "block";
   }
 
-  if (shouldShowPopup2()) {
-    // 예원: 팝업 안보이게 처리 (2026-03-12) show : "block", hide: "none"
-    document.getElementById("popup2").style.display = "block";
-  }
+  // if (shouldShowPopup2()) {
+  //   document.getElementById("popup2").style.display = "block";
+  // }
 
   updateContainerVisibility();
 };
